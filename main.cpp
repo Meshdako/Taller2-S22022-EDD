b24c63d4037339d922f809f32d1f81ecce5ff3fe
Meshdako <msobarzo@utem.cl>
2022-10-08 12:22:47 -0300

    archivo main creado, debe ser modificado

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+/**
+ * INFORMACIÓN DEL ELECTOR
+ * RUN
+ * Nombre
+ * Fecha Nacimiento
+ * Dirección
+ * Comuna
+ * Habilitado para sufragar
+ * Es vocal de mesa
+ * Mesa de Votación
+ */
+
+int main()
+{
+    cout << "Hello World" << endl;
+    return 0;
+}
