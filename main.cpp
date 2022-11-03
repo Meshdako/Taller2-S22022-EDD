ed6640b76a62e76458d16f775dde935bd36f37c6
meshdako <msobarzo@utem.cl>
2022-11-03 03:28:35 -0300

    main actualizado para la creación de la Cola y la muestra de esta.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 
+#include "Funciones.h"
+#include "Funciones.cpp"
+
 using namespace std;
+
+//Clases
+#include "Clases/Direccion.h"
+#include "Clases/Direccion.cpp"
+#include "Clases/Fecha.h"
+#include "Clases/Fecha.cpp"
+#include "Clases/Run.h"
+#include "Clases/Run.cpp"
+#include "Clases/Persona.h"
+#include "Clases/Persona.cpp"
+#include "Clases/Elector.h"
+#include "Clases/Elector.cpp"
+
+//TAD
+#include "TAD/Cola.h"
+#include "TAD/Stack.h"
+
+#define MAX_VECTOR 1000
 
 /**
  * INFORMACIÓN DEL ELECTOR
@@ -16,6 +37,16 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World" << endl;
+    int indice = 0; //indice del vector  o cantidad actual de elementos en el vector
+    string nombreArchivo ="servel_pruebas.txt";
+    
+    Elector Electores[MAX_VECTOR];
+
+    Cola<Elector> Cola_Electores;
+
+    archivoAVector(nombreArchivo, Electores, indice);
+    arregloACola(Cola_Electores, Electores, indice);
+    mostrarCola(Cola_Electores);
+
     return 0;
 }
