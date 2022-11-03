19d7e04b86d611c4884fb4c57d1f338c54bb80a2
meshdako <msobarzo@utem.cl>
2022-11-03 04:16:50 -0300

    Agregadas todas las funciones que serán trabajadas para el Menú de opciones.

--- Funciones.h
+++ Funciones.h
@@ -17,8 +17,37 @@
 
 using namespace std;
 
+//Implementa correctamente una función que recibe una Cola y un run, y muestra en pantalla todos los datos del elector.
+void ConsultaXRun(Cola<Elector> &C, int in_Run);
+
+//Implementa correctamente una función que recibe una Cola y un string y retorna un Stack con la lista de electores de dicha comuna.
+Stack<Elector> ElectoresXComuna(Cola<Elector> &C, string in_Comuna);
+
+//Muestra en pantalla el número de electores que hayan nacido en el año que indique el usuario.
+void RunXAnhio(Cola<Elector> &C, int in_Anhio);
+
+//implementa correctamente una función que recibe una cola y un carácter, y retorna un Stack con los electores que se encuentran inhabilitados para sufragar cuyo apellido paterno comience con el carácter indicado.
+Stack<Elector> InhabilitadosXLetra(Cola<Elector> &C, char in_Letra);
+
+/**
+ * Implementa correctamente las funciones necesarias para mostrar los siguientes datos en pantalla:
+ * total de electores
+ * total de electores cuyo año de nacimiento es mayor a 2002
+ * total de electores habilitados para votar mayores de 35 años
+ * total de vocales de mesa de la comuna de Macul.
+*/
+void Totalizador(Cola<Elector> &C);
+
+//Menú que despliega todas las funciones de forma ordenada.
+void Menu();
+
+//Función que muestra una pila.
+void mostrarPila(Stack<Elector> &S);
+
+//Función que muestra la Cola, sin modificarla.
 void mostrarCola(Cola<Elector> &C);
 
+//Función que pasa un arreglo a una Cola.
 void arregloACola(Cola<Elector> &C, Elector p[], int indice);
 
 //Función que lee el archivo "servel" y guarda la información en un arreglo.
